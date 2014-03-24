2c3888a50096800299f1df578d19edd773982152
Scott Wilcox <scott@dor.ky>
2014-03-24 20:51:29 +0000

    Adding Spanish translation thanks to Andrea Balado

---
+++ es/index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Replace the description, keywords and author content with those that suit your product/ service -->
+    <meta name="description" content="Queremos que seas parte de la evolución de tweekly.fm">
+    <meta name="author" content="Scott Wilcox">
+
+    <title>Tweekly.fm &middot; Queremos que seas parte de la evolución de tweekly.fm</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="/assets/cust/css/cust.css?<?php echo time(); ?>" rel="stylesheet" media="screen">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/assets/font-awesome/css/font-awesome.min.css">
+    <!-- Load this instead, on live site
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet"> -->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body id="home" class="cornflowerblue"> <!-- The "cornflowerblue" class adds a stylish blue color scheme -->
+  <?php require "../language.php"; ?>
+    <!-- Jumbotron -->
+    <div class="jumbotron">
+      <div class="container">
+
+        <!-- Intro -->
+        <div class="row">
+          <div class="col-sm-10 col-sm-offset-1">
+            <h1>Queremos que seas parte de la evolución de tweekly.fm</h1>
+            <p class="lead">Necesitamos tu ayuda para continuar trabajando y mejorando tweekly.fm y llevarlo al próximo nivel, ofreciendo un mejor servicio y funciones para nuestros usuarios.</p>
+          </div>
+        </div> <!-- End intro -->
+
+        <!-- Assets (How to's) -->
+        <div class="row assets hidden-xs">
+          <div class="col-sm-4">
+            <div class="asset">
+              <span class="fa fa-twitter asset-ico-a"></span>
+              <h3>Más de 20 millones de actualizaciones publicadas</h3>
+              <p>Desde enero de 2011, se han publicado más de 20 millones de actualizaciones en las redes sociales.</p>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="asset">
+              <span class="fa fa-sitemap asset-ico-b"></span>
+              <h3>3.1 millones de visitas</h3>
+              <p>Nuestros usuarios y perfiles han generado más de 3.1 millones de visitas.</p>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="asset">
+              <span class="fa fa-rocket asset-ico-c"></span>
+              <h3>Hacia un millón de usuarios</h3>
+              <p>Recientemente superamos la marca de medio millón de usuarios registrados desde enero de 2011.</p>
+            </div>
+          </div>
+        </div> <!-- End assets -->
+      </div>
+    </div> <!-- End Jumbotron -->
+
+    <!-- Main Content -->
+    <div class="container">
+
+      <!-- Features -->
+      <div class="features">
+
+        <!-- Section header -->
+        <div class="row section-header">
+          <div class="col-sm-12" style="color: #747474 !important; text-align: justify; border-bottom: 2px solid #ccc; margin-bottom: 20px;">
+				<!-- AddThis Button BEGIN -->
+				<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+				<a class="addthis_button_preferred_1"></a>
+				<a class="addthis_button_preferred_2"></a>
+				<a class="addthis_button_preferred_3"></a>
+				<a class="addthis_button_preferred_4"></a>
+				<a class="addthis_button_compact"></a>
+				<a class="addthis_counter addthis_bubble_style"></a>
+				</div>
+				<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+				<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-532ccb98090d447f"></script>
+				<!-- AddThis Button END -->
+
+            <h1>¿Por qué necesitamos tu ayuda?</h1>
+<p>En diciembre de 2010 escribí un simple programa que tomaría mis artistas más escuchados de la semana y lo publicaría en Twitter. Unos años y 15 millones de actualizaciones después, Tweekly.fm publica diariamente para miles de personas. Nunca hubiera imaginado que gente trabajando en Twitter, Google, Last.fm y otras empresas grandes de tecnología usarían mi pequeño proyecto.</p>
+
+<p>Todo este tiempo el proyecto ha sido desarrollado y financiado exclusivamente por mí. El tiempo de desarrollo sobrepasa fácilmente las cientos de horas y el soporte toma alrededor de seis horas a la semana en promedio. Recientemente, he estado programando la próxima versión de tweekly.fm para que incluya nuevos perfiles, publicaciones en tumblr y Wordpress y otras redes sociales, así como una mejor experiencia para los usuarios actuales.</p>
+
+<p>Tengo un trabajo a tiempo completo además de trabajar como autónomo para cubrir mis gastos de vida. Quisiera poder dejar algunos trabajos a un lado y dedicar mi tiempo exclusivamente a tweekly.fm.</p>
+
+<p>Anteriormente, cuando hemos necesitado ayuda para el alojamiento, gente muy amable en Last.fm ofreció su ayuda. Esto no es algo que una compañía externa pueda resolver, aunque el servicio de alojamiento haya sido efectivamente financiado por mi propia compañía en su totalidad. En el futuro, espero que Tweekly.fm pueda ser independiente y sostenible y eventualmente genere ganancias. En ese punto, podría enfocarme exclusivamente en ello.</p>
+
+<p>Escribo esto para pedirte que dones cualquier monto que consideres correcto para el desarrollo de tweekly.fm y nos ayudes con el período de pruebas. También me gustaría conocer tu visión y tus opiniones en cuanto a lo que debemos hacer a continuación con tweekly.fm</p>
+
+<p>Puedes tomar una encuesta corta o donar utilizando los enlaces en la parte de abajo.</p>
+
+<p>Me gustaría publicar la lista de colaboradores en el sitio pero si no deseas aparecer en la lista, envíame un correo electrónico y házmelo saber. Para darte las gracias, todos los colaboradores recibirán una cuenta de por vida en la nueva versión de tweekly.fm.</p>
+
+<p>Si hay algo que quisieras discutir del servicio, planes o ideas, por favor escríbeme a <a href="mailto:support@tweekly.fm">support@tweekly.fm</a>.</p>
+          </div>
+        </div> <!-- End section header -->
+
+        <!-- Second row of features -->
+        <div class="row" style="color: #747474 !important;">
+          <div class="col-sm-6">
+            <h3><i class="fa fa-tumblr"></i> ¿Qué podemos hacer?</h3>
+            <p>Puedes completar nuestra encuesta <a href="http://q.tweekly.fm/take_survey.php?x=NTQzMjI%3D&utm_source=help-fundraiser&utm_medium=website&utm_campaign=help.tweekly.fm">aquí</a>. Sólo toma 30 segundos.</p>
+            <p>Las respuestas de la encuesta ayudarán a decidir el futuro de tweekly.fm.</p>
+          </div>
+
+          <div class="col-sm-6">
+            <h3><i class="fa fa-user"></i> ¡Gracias!</h3>
+			<p>Quiero agradecer a las siguientes personas por ayudar con las traducciones:<br /></p>
+			<ul>
+				<?php require "../translators.php"; ?>
+			</ul>
+          </div>
+        </div> <!-- End second row of features -->
+
+
+        <div class="row" style="color: #747474 !important;">
+          <div class="col-sm-12">
+          	&nbsp;<br /><br />
+          </div>
+        </div>
+
+        <div class="row" style="color: #747474 !important;">
+          <div class="col-sm-12">
+          	&nbsp;<br /><br />
+          </div>
+        </div>
+
+
+        <!-- Second row of features -->
+        <div class="row" style="color: #747474 !important;">
+          <div class="col-sm-3">
+            <h3><i class="fa fa-credit-card"></i> Donar a través de PayPal (USD - Dólares americanos)</h3>
+			<div style="text-align: center">
+				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="margin-left: auto; margin-right: auto;">
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<input type="hidden" name="hosted_button_id" value="VZSSCFURSL7MA">
+					<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
+					<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+				</form>
+				<p><small>También puedes donar directamente a support@tweekly.fm</small></p>
+			</div>
+          </div>
+
+          <div class="col-sm-3">
+            <h3><i class="fa fa-credit-card"></i> Donar a través de PayPal (GBP - Libras esterlinas)</h3>
+			<div style="text-align: center">
+				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<input type="hidden" name="hosted_button_id" value="9S453HKMPDSJN">
+					<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
+					<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+				</form>
+				<p><small>También puedes donar directamente a support@tweekly.fm</small></p>
+			</div>
+          </div>
+
+          <div class="col-sm-3">
+            <h3><i class="fa fa-bitcoin"></i> Donar Bitcoin</h3>
+			<div style="text-align: center">
+				<script src="http://coinwidget.com/widget/coin.js"></script>
+				<script>
+				CoinWidgetCom.go({
+					wallet_address: "1CgAZUsHnae26mQXPBjse2CqTFPPuiNX1R"
+					, currency: "bitcoin"
+					, counter: "hidden"
+					, alignment: "bl"
+					, qrcode: true
+					, auto_show: false
+					, lbl_button: "Donate"
+					, lbl_address: "My Bitcoin Address:"
+					, lbl_count: "donations"
+					, lbl_amount: "BTC"
+				});
+				</script>
+			</div>
+          </div>
+
+          <div class="col-sm-3">
+            <h3><i class="fa fa-money"></i> Donar Litecoin</h3>
+			<div style="text-align: center">
+				<script src="http://coinwidget.com/widget/coin.js"></script>
+				<script>
+				CoinWidgetCom.go({
+					wallet_address: "LdxMrzTRYKtDNnDQukUBkMzxLkZ1MnDGSC"
+					, currency: "litecoin"
+					, counter: "hidden"
+					, alignment: "bl"
+					, qrcode: true
+					, auto_show: false
+					, lbl_button: "Donate"
+					, lbl_address: "My Litecoin Address:"
+					, lbl_count: "donations"
+					, lbl_amount: "LTC"
+				});
+				</script>
+			</div>
+          </div>
+        </div> <!-- End second row of features -->
+
+      </div> <!-- End features -->
+
+      <!-- Footer -->
+      <div class="footer">
+        <div class="row" style="color: #747474 !important;">
+          <div class="col-sm-8">
+          	<h3>Gracias por leer</h3>
+            <p>
+				De mi parte, muchas gracias por leer esta página. Aunque decidas no colaborar con el proyecto de alguna forma, aprecio que te hayas tomado el tiempo para leer este mensaje. En mi opinión, eso demuestra que tweekly.fm tiene una influencia positiva en tu vida.
+            </p>
+
+            <p>
+            	<strong><a href="http://dor.ky">Scott Wilcox</a> (<a href="http://twitter.com/ssx">@ssx</a>)</strong><br />
+            	Tweekly.fm
+            </p>
+          </div>
+        </div>
+      </div> <!-- End footer -->
+    </div>
+	<script src="http://code.jquery.com/jquery.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="/assets/cust/js/cust.js"></script>
+
+   <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46716686-3', 'tweekly.fm');
+  ga('send', 'pageview');
+
+</script>
+  </body>
+</html>
